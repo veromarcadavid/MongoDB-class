5ca491778c0e0eeac6e6c468a0a12176a297a2d1
Veronica Martinez Cadavid <144962625+veromarcadavid@users.noreply.github.com>
2023-10-11 15:57:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,8 @@ const getNestedRecordsExample = () => [
     }
 ];
 
+//RETO 1: SUMAR MATRÍCULAS
+
 function sumar(arr){
   var total=arr.reduce(function(accumulator, currentElement){
   return accumulator + currentElement;
@@ -60,8 +62,52 @@ const arr= getNestedRecordsExample();
 
 var arrayMatricula=[];
 
-arr.forEach((currentElement)=> arrayMatricula.push((Object.values(currentElement)[2].valorMatricula)));
+arr.forEach(
+    (currentElement)=> arrayMatricula.push
+    ((Object.values(currentElement)[2].valorMatricula)
+));
 
 const suma = sumar(arrayMatricula);
 
 document.getElementById('output').innerHTML = "La suma de las matriculas es: $" + suma;
+
+//RETO 2: sobre la misma data o función en un array obtener los datos de contacto teléfono de los estudiantes
+
+var arrayContacto=[];
+
+arr.forEach((
+    currentElement)=> arrayContacto.push
+    ((Object.values(currentElement)[2].contacto.telefono)
+));
+
+document.getElementById('output1').innerHTML = "Los números de télefono son: " + arrayContacto;
+
+//RETO 3: obtener el nombre del estudiante con mayor valor de matricula
+
+var maxValue = Math.max(...arrayMatricula);
+
+const estudiante = arr.filter(
+    registro => registro.detalles.valorMatricula === maxValue
+)
+
+function getNombre(item) {
+  return [item.nombre];
+}
+
+const nombreEstudiante = estudiante.map(getNombre);
+
+document.getElementById('output2').innerHTML = 
+"El estudiante " + nombreEstudiante[0] + " tiene la matricula mas alta, con valor $" +maxValue;
+
+//RETO 4: Obtener el correo del estudiante con matricula condicional y RETO 5: Obtener 
+//el correo del estudiante con matricula condicional, considerar los casos en que en 
+//el array no haya estudiantes con matricula condicional o que halla mas de uno
+
+const estudianteMatricula = arr.filter(
+    registro => registro.detalles.descripcion === 'matricula condicional'
+)
+
+const nombreMatricula = estudianteMatricula.map(getNombre);
+
+document.getElementById('output3').innerHTML = "Estudiantes con matrícula condicional: " + nombreMatricula;
+
